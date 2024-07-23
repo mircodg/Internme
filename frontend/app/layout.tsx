826d5dde9e060d4905e202997129ce4c0c26db1d
Domenico Germanà <Mirkogermana12@gmail.com>
2024-07-23 20:08:51 +0200

    fix: moved mode toggle and landing navbar in landing Layout file

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider";
-import LandingNavbar from "@/components/LandingNavbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,21 +15,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <>
-      <html lang="en" suppressHydrationWarning>
-        <head />
-        <body>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            <LandingNavbar />
-            {children}
-          </ThemeProvider>
-        </body>
-      </html>
-    </>
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body>{children}</body>
+    </html>
   );
 }
