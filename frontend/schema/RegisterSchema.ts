911296df85499bf7d89542002aa7488d11e5b131
Domenico Germanà <Mirkogermana12@gmail.com>
2024-07-28 11:47:33 +0200

    feat: added zod schema for any type of registration

--- frontend/schema/RegisterSchema.ts
+++ frontend/schema/RegisterSchema.ts
@@ -1,27 +1,46 @@
 import * as z from "zod";
 
-export const RegisterSchema = z
-  .object({
-    firstName: z.string().min(1, {
-      message: "Please enter your name",
-    }),
-    lastName: z.string().min(1, {
-      message: "Please enter your last name",
-    }),
-    email: z.string().email({
-      message: "Please enter a valid email address",
-    }),
-    password: z.string().min(8, {
-      message: "password must be at least 8 characters long",
-    }),
-    confirmPassword: z.string(),
-  })
-  .refine(
-    (values) => {
-      return values.password === values.confirmPassword;
-    },
-    {
-      message: "Passwords must match!",
-      path: ["confirmPassword"],
-    }
-  );
+export const studentRegisterSchema = z.object({
+  collegeName: z.string().min(1, {
+    message: "Please enter the college name where you study",
+  }),
+  studentID: z.string().min(6, {
+    message: "Please enter a valid student ID",
+  }),
+  firstName: z.string().min(1, {
+    message: "Please enter your name",
+  }),
+  lastName: z.string().min(1, {
+    message: "Please enter your last name",
+  }),
+  email: z.string().email({
+    message: "Please enter a valid email address",
+  }),
+  password: z.string().min(8, {
+    message: "Password must be at least 8 characters long",
+  }),
+});
+
+export const academicRegisterSchema = z.object({
+  collegeName: z.string().min(1, {
+    message: "Please enter the college name",
+  }),
+  email: z.string().email({
+    message: "Please enter a valid email address",
+  }),
+  password: z.string().min(8, {
+    message: "Password must be at least 8 characters long",
+  }),
+});
+
+export const businessRegisterSchema = z.object({
+  companyName: z.string().min(1, {
+    message: "Please enter the company name",
+  }),
+  email: z.string().email({
+    message: "Please enter a valid email address",
+  }),
+  password: z.string().min(8, {
+    message: "Password must be at least 8 characters long",
+  }),
+});
