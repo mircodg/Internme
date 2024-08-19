9396266fa05e2790606c10921d3c8e3b01b0a248
Domenico Germanà <Mirkogermana12@gmail.com>
2024-08-19 18:38:06 +0200

    feat: added register route for user registration

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,10 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\StudentsController;
+use App\Http\Controllers\UserController;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/students', [StudentsController::class, 'index']);
+
+
+Route::post('/register', [UserController::class, 'register']);
+Route::get('/user/{email}', [UserController::class, 'getUserByEmail']);
