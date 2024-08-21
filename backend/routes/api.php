d3c6c484b0d8f087d7cee53b76db3ac45d5090a6
Domenico Germanà <Mirkogermana12@gmail.com>
2024-08-21 23:30:23 +0200

    feat: added auth routes and protected with custom middleware + sanctum

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,15 +1,33 @@
 <?php
 
-use Illuminate\Http\Request;
+// use Illuminate\Support\Facades\Route;
+// use App\Http\Controllers\UserController;
+// use App\Http\Controllers\AuthController;
+
+
+
+
+
+// Route::post('/register', [AuthController::class, 'register']);
+// Route::post('/login', [AuthController::class, 'login']); 
+
+
+// Route::middleware(['jwt.cookie'])->group(function () {
+//     Route::get('/user/{email}', [UserController::class, 'getUserByEmail'])->middleware('auth:sanctum');
+// });
+
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\AuthController;
+use App\Http\Middleware\JwtCookieToBearer;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+ Route::middleware([JwtCookieToBearer::class], 'auth:sanctum')->group(function () {
+     Route::get('/user/{email}', [UserController::class, 'getUserByEmail']);
+     Route::post('/logout', [AuthController::class, 'logout']);
+});
 
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-
-
-Route::post('/register', [UserController::class, 'register']);
-Route::get('/user/{email}', [UserController::class, 'getUserByEmail']);
