4728c8c3b552410a10364b1a38730aa7a84a54a3
Domenico Germanà <Mirkogermana12@gmail.com>
2024-08-24 20:01:03 +0200

    feat: added redirect button to correct dash if user is logged

--- frontend/components/Landing/LandingNavbar.tsx
+++ frontend/components/Landing/LandingNavbar.tsx
@@ -2,23 +2,38 @@ import Link from "next/link";
 import { Button } from "../ui/button";
 import { ModeToggle } from "../ToggleTheme";
 
-const LandingNavbar = () => {
+interface LandingNavbarProps {
+  isAuthenticated: boolean;
+  role?: string;
+}
+
+const LandingNavbar = ({ isAuthenticated, role }: LandingNavbarProps) => {
   return (
     <nav className="h-14 flex justify-between w-full px-2.5 md:px-20 max-w-screen-2xl items-center mx-auto">
       <Link href="/">
         <p className="text-balance text-center font-bold">internMe</p>
       </Link>
       <div className="flex gap-4 items-center justify-center">
-        <Link href="/auth/login" className=" hidden md:block">
-          <Button variant={"outline"} size={"sm"} className="font-bold">
-            Login
-          </Button>
-        </Link>
-        <Link href="/auth/register" className=" hidden md:block">
-          <Button variant={"outline"} size={"sm"} className="font-bold">
-            Register
-          </Button>
-        </Link>
+        {!isAuthenticated ? (
+          <>
+            <Link href="/auth/login" className=" hidden md:block">
+              <Button variant={"outline"} size={"sm"} className="font-bold">
+                Login
+              </Button>
+            </Link>
+            <Link href="/auth/register" className=" hidden md:block">
+              <Button variant={"outline"} size={"sm"} className="font-bold">
+                Register
+              </Button>
+            </Link>
+          </>
+        ) : (
+          <Link href={`/dashboard/${role}`} className=" hidden md:block">
+            <Button variant={"outline"} size={"sm"} className="font-bold">
+              Dashboard
+            </Button>
+          </Link>
+        )}
         <ModeToggle />
       </div>
     </nav>
