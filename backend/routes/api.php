1d9728cc0cdf6e551ecb98a2fc399f375d96f73e
Domenico Germanà <Mirkogermana12@gmail.com>
2024-08-25 19:57:49 +0200

    feat: added user route to retrive user based on cookie sent

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,22 +1,5 @@
 <?php
 
-// use Illuminate\Support\Facades\Route;
-// use App\Http\Controllers\UserController;
-// use App\Http\Controllers\AuthController;
-
-
-
-
-
-// Route::post('/register', [AuthController::class, 'register']);
-// Route::post('/login', [AuthController::class, 'login']); 
-
-
-// Route::middleware(['jwt.cookie'])->group(function () {
-//     Route::get('/user/{email}', [UserController::class, 'getUserByEmail'])->middleware('auth:sanctum');
-// });
-
-
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AuthController;
@@ -28,6 +11,7 @@ Route::post('/login', [AuthController::class, 'login']);
  Route::middleware([JwtCookieToBearer::class], 'auth:sanctum')->group(function () {
      Route::get('/user/{email}', [UserController::class, 'getUserByEmail']);
      Route::post('/logout', [AuthController::class, 'logout']);
+     Route::get('/user', [UserController::class, 'returnUser']);
 });
 
 
