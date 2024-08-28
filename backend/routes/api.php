564a3f8259371afe9bbc8292737eb3ccd023467b
Domenico Germanà <Mirkogermana12@gmail.com>
2024-08-28 15:37:35 +0200

    feat: added routes for universita

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AuthController;
 use App\Http\Middleware\JwtCookieToBearer;
+use App\Http\Controllers\UniversitaController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
@@ -12,6 +13,10 @@ Route::post('/login', [AuthController::class, 'login']);
      Route::get('/user/{email}', [UserController::class, 'getUserByEmail']);
      Route::post('/logout', [AuthController::class, 'logout']);
      Route::get('/user', [UserController::class, 'returnUser']);
+     Route::post('/university', [UniversitaController::class, 'addUniversita']);
+     Route::get('/university', [UniversitaController::class, 'getUni']);
+     Route::get('/university/students', [UniversitaController::class, 'getStudentsCount']);
+     
 });
 
 
