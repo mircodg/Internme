8047fb9e365432b0ad36d0da05d2b613dc787aed
Domenico Germanà <Mirkogermana12@gmail.com>
2024-08-29 17:07:58 +0200

    feat: added zod schema validation for student, company and university creation

--- frontend/schema/DialogSchema.ts
+++ frontend/schema/DialogSchema.ts
@@ -1,15 +1,28 @@
 import * as z from "zod";
 
 export const academicDialogSchema = z.object({
-  Nome: z.string().min(1, { message: "Please enter a valid name" }),
+  NomeUniversita: z.string().min(1, { message: "Please enter a valid name" }),
   Via: z.string().min(1, { message: "Please enter a valid route" }),
   NumeroCivico: z.preprocess(
     (val) => (val === "" ? undefined : parseInt(val as string)),
-    z.number().int().min(1, { message: "Please enter a valid street number" })
+    z
+      .number()
+      .int()
+      .refine((val) => val >= 0, {
+        message: "Please enter a valid street number",
+      })
   ),
-  CAP: z.string().min(1, { message: "Please enter a valid postal code" }),
+  CAP: z.preprocess(
+    (val) => (val === "" ? undefined : parseInt(val as string)),
+    z
+      .number()
+      .int()
+      .refine((val) => val.toString().length === 5)
+  ),
   Citta: z.string().min(1, { message: "Please enter a valid city" }),
-  Provincia: z.string().min(1, { message: "Please enter a valid province" }),
+  Provincia: z.string().refine((val) => val.length === 2, {
+    message: "Please enter a valid province",
+  }),
 });
 
 export const businessDialogSchema = z.object({
@@ -19,18 +32,47 @@ export const businessDialogSchema = z.object({
     .min(1, { message: "Please enter a valid work sector" }),
   PartitaIva: z.preprocess(
     (val) => (val === "" ? undefined : parseInt(val as string)),
-    z.number().int().min(11, { message: "Please enter a valid VAT number" })
+    z
+      .number()
+      .int()
+      .refine((val) => val.toString().length === 11)
   ),
   Via: z.string().min(1, { message: "Please enter a valid route" }),
   NumeroCivico: z.preprocess(
     (val) => (val === "" ? undefined : parseInt(val as string)),
-    z.number().int().min(1, { message: "Please enter a valid street number" })
+    z
+      .number()
+      .int()
+      .refine((val) => val >= 0, {
+        message: "Please enter a valid street number",
+      })
   ),
-  CAP: z.string().min(1, { message: "Please enter a valid postal code" }),
+  CAP: z.preprocess(
+    (val) => (val === "" ? undefined : parseInt(val as string)),
+    z
+      .number()
+      .int()
+      .refine((val) => val.toString().length === 5)
+  ),
   Citta: z.string().min(1, { message: "Please enter a valid city" }),
-  Provincia: z.string().min(1, { message: "Please enter a valid province" }),
+  Provincia: z.string().refine((val) => val.length === 2, {
+    message: "Please enter a valid province",
+  }),
 });
 
 export const studentDialogSchema = z.object({
-    
-}); 
+  Matricola: z.preprocess(
+    (val) => parseInt(val as string),
+    z
+      .number()
+      .int()
+      .refine((val) => val.toString().length === 6, {
+        message: "Please enter a valid student ID",
+      })
+  ),
+  Universita: z.string().min(1, { message: "Please enter a valid university" }),
+  CorsoDiLaurea: z.string().min(1, {
+    message: "Please enter a valid degree course",
+  }),
+  CV: z.any(),
+});
