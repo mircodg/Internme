3cf7b3108d88b4bbe109c89259b027e56fc7a751
Domenico Germanà <Mirkogermana12@gmail.com>
2024-08-29 16:56:17 +0200

    fix: small bug fixes

--- backend/app/Models/Universita.php
+++ backend/app/Models/Universita.php
@@ -6,43 +6,45 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
 use PDO;
-use PDOException; 
+use PDOException;
 use Symfony\Component\HttpFoundation\Response;
-use App\Models\Utente; 
+use App\Models\Utente;
 
 
 class Universita extends Model
 {
-    use HasFactory; 
+    use HasFactory;
 
-    protected $pdo; 
+    protected $pdo;
     protected $table = 'Università';
     protected $primaryKey = 'idUniversita';
 
-    public function __construct(){
+    public function __construct()
+    {
         $this->pdo = DB::connection()->getPdo();
     }
 
-    public function insertUniversita($nomeUniversita, $token){
-        try{
+    public function insertUniversita($nomeUniversita, $token)
+    {
+        try {
             $user = new Utente();
             $response = $user->getUserByToken($token);
             $data = $response->getData(true);
 
-            if($data['user']['TipoUtente'] != 'Direttore'){
+            if ($data['user']['TipoUtente'] != 'Direttore') {
                 return response()->json([
                     'message' => 'You are not a Director'
                 ], Response::HTTP_UNAUTHORIZED);
             }
-            
+
             $userID = $data['user']['idUtente'];
 
-            $sql = "INSERT INTO Universita (Nome, idUtente) VALUES (:nomeUniversita, :idUtente)"; 
+            $sql = "INSERT INTO Universita (Nome, idUtente) VALUES (:nomeUniversita, :idUtente)";
             $stmnt = $this->pdo->prepare($sql);
             $stmnt = $stmnt->execute([
                 'nomeUniversita' => $nomeUniversita,
                 'idUtente' => $userID
-            ]); 
+            ]);
             $sql = "SELECT * FROM Universita WHERE Nome = :nomeUniversita";
             $stmnt = $this->pdo->prepare($sql);
             $stmnt->execute([
@@ -53,22 +55,23 @@ class Universita extends Model
                 'message' => 'University created successfully',
                 'university' => $newUni
             ], Response::HTTP_OK);
-        } catch (PDOException $e){
+        } catch (PDOException $e) {
             echo $e->getMessage();
             return response()->json([
-                'message' => 'Error while creating university', 
+                'message' => 'Error while creating university',
                 'error' => $e->getMessage()
             ], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
 
-    public function getUniFromToken($token){
-        try{
+    public function getUniFromToken($token)
+    {
+        try {
             $user = new Utente();
             $response = $user->getUserByToken($token);
             $data = $response->getData(true);
 
-            if($data['user']['TipoUtente'] != 'Direttore'){
+            if ($data['user']['TipoUtente'] != 'Direttore') {
                 return response()->json([
                     'message' => 'You are not a Director'
                 ], Response::HTTP_UNAUTHORIZED);
@@ -85,33 +88,80 @@ class Universita extends Model
             return response()->json([
                 'university' => $universita
             ], Response::HTTP_OK);
-        } catch (PDOException $e){
+        } catch (PDOException $e) {
             echo $e->getMessage();
             return response()->json([
-                'message' => 'Error while getting university', 
+                'message' => 'Error while getting university',
                 'error' => $e->getMessage()
             ], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
 
-    public function studentsCount($token){
-        try{
-            $universita = new Universita(); 
+    //TODO: Sposta questa funzione in un model Studente
+    public function studentsCount($token)
+    {
+        try {
+            $universita = new Universita();
             $response = $universita->getUniFromToken($token);
             $data = $response->getData(true);
-            
-            $sql = "SELECT * FROM Studenti WHERE idUniversita = :idUniversita"; 
-            $stmnt = $this->pdo->prepare(($sql)); 
+
+            $sql = "SELECT * FROM Studenti WHERE idUniversita = :idUniversita";
+            $stmnt = $this->pdo->prepare(($sql));
             $stmnt->execute([
                 'idUniversita' => $data['university']['idUniversita']
             ]);
             return response()->json([
-                'Students' => $stmnt->rowCount()
+                'message' => 'Student count retrieved successfully',
+                'count' => $stmnt->rowCount()
             ], Response::HTTP_OK);
-        } catch (PDOException $e){
+        } catch (PDOException $e) {
             echo $e->getMessage();
             return response()->json([
-                'message' => 'Error while getting student count', 
+                'message' => 'Error while getting student count',
+                'error' => $e->getMessage()
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    function showAll()
+    {
+        try {
+            $sql = "SELECT * FROM Universita";
+            $stmnt = $this->pdo->prepare($sql);
+            $stmnt->execute();
+            $universita = $stmnt->fetchAll(PDO::FETCH_ASSOC);
+            return response()->json([
+                'universities' => $universita
+            ], Response::HTTP_OK);
+        } catch (PDOException $e) {
+            return response()->json([
+                'message' => 'Error while getting universities',
+                'error' => $e->getMessage(),
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    function getUniIDByName($nomeUniversita)
+    {
+        try {
+            $sql = "SELECT * FROM Universita WHERE Nome = :nomeUniversita";
+            $stmnt = $this->pdo->prepare($sql);
+            $stmnt->execute([
+                'nomeUniversita' => $nomeUniversita
+            ]);
+            if ($stmnt->rowCount() == 0) {
+                return response()->json([
+                    'message' => 'University not found'
+                ], Response::HTTP_NOT_FOUND);
+            } else {
+                $universita = $stmnt->fetch(PDO::FETCH_ASSOC);
+                return response()->json([
+                    'university' => $universita
+                ], Response::HTTP_OK);
+            }
+        } catch (PDOException $e) {
+            return response()->json([
+                'message' => 'Error while getting university',
                 'error' => $e->getMessage()
             ], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
