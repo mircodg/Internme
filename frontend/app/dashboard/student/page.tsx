88e65a132cdd78e2f929d3e5916f7062fbc8489e
Domenico Germanà <Mirkogermana12@gmail.com>
2024-08-29 17:10:34 +0200

    feat: added student dash page

--- frontend/app/dashboard/student/page.tsx
+++ frontend/app/dashboard/student/page.tsx
@@ -1,7 +1,12 @@
+import StudentDashboard from "@/components/dash-components/studentDashboard";
 import React from "react";
 
 function page() {
-  return <div>STUDENT BITCH</div>;
+  return (
+    <div>
+      <StudentDashboard />
+    </div>
+  );
 }
 
 export default page;
