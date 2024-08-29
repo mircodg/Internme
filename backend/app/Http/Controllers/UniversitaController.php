6f5d967b62603f13b2e0fe22fce8c97d69019acb
Domenico Germanà <Mirkogermana12@gmail.com>
2024-08-29 17:01:50 +0200

    feat: added method to get companies count

--- backend/app/Http/Controllers/UniversitaController.php
+++ backend/app/Http/Controllers/UniversitaController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Universita;
-use App\Models\IndirizziUniversita; 
+use App\Models\Azienda;
+use App\Models\IndirizziUniversita;
 
 class UniversitaController extends Controller
 {
-    function addUniversita(Request $req){
+    function addUniversita(Request $req)
+    {
         $universita = new Universita();
         $indirizzoUni = new IndirizziUniversita();
         $response = $universita->insertUniversita($req->NomeUniversita, $req->cookie('jwt'));
@@ -16,13 +18,30 @@ class UniversitaController extends Controller
         return $indirizzoUni->insertUniAddress($req->Via, $req->NumeroCivico, $req->CAP, $req->Citta, $req->Provincia, $newUni['university']['idUniversita']);
     }
 
-    function getUni(Request $req){
+    function getUni(Request $req)
+    {
         $universita = new Universita();
         return $universita->getUniFromToken($req->cookie('jwt'));
     }
 
-    function getStudentsCount(Request $req){
+    function getStudentsCount(Request $req)
+    {
         $universita = new Universita();
         return $universita->studentsCount($req->cookie('jwt'));
+    }
+
+    function getCompaniesCount(Request $req)
+    {
+        $universita = new Universita();
+        $response = $universita->getUniFromToken($req->cookie('jwt'));
+        $data = $response->getData(true);
+        $idUniversita = $data['university']['idUniversita'];
+        //TODO: get companies count through model Stipulazioni 
+    }
+
+    function showAllUni()
+    {
+        $universita = new Universita();
+        return $universita->showAll();
     }
 }
