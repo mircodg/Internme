6e9c2646e1ff1de66993636e255983070e70fc78
Domenico Germanà <Mirkogermana12@gmail.com>
2024-09-17 10:02:14 +0200

    feat: added zod schema for address validation

--- frontend/schema/DialogSchema.ts
+++ frontend/schema/DialogSchema.ts
@@ -76,3 +76,27 @@ export const studentDialogSchema = z.object({
   }),
   CV: z.any(),
 });
+
+export const addressSchema = z.object({
+  Via: z.string().min(1, { message: "Please enter a valid route" }),
+  NumeroCivico: z.preprocess(
+    (val) => (val === "" ? undefined : parseInt(val as string)),
+    z
+      .number()
+      .int()
+      .refine((val) => val >= 0, {
+        message: "Please enter a valid street number",
+      })
+  ),
+  CAP: z.preprocess(
+    (val) => (val === "" ? undefined : parseInt(val as string)),
+    z
+      .number()
+      .int()
+      .refine((val) => val.toString().length === 5)
+  ),
+  Citta: z.string().min(1, { message: "Please enter a valid city" }),
+  Provincia: z.string().refine((val) => val.length === 2, {
+    message: "Please enter a valid province",
+  }),
+});
